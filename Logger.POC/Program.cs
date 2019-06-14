726bc2dea14b3d30d310cc9be8602da83fd43bd5
Md Javed <Md.Javed@Indellient.com>
2019-06-14 11:04:30 -0400

    Add project files.

---
+++ Logger.POC/Program.cs
@@ -0,0 +1,19 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loggers.POC
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Error("This is an error message");
+            Console.Read();
+        }
+    }
+}
