fda828ca54960369153ee53399a3cb42ccada670
Md Javed <Md.Javed@Indellient.com>
2019-07-03 08:42:56 -0400

    encryption and decryption addded

--- Logger.POC/Program.cs
+++ Logger.POC/Program.cs
@@ -1,6 +1,7 @@
 ﻿using NLog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,38 @@ namespace Loggers.POC
         private static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            logger.Error("This is an error message");
+            var GetDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+            Console.WriteLine(GetDirectory);
+
+            {
+                var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+               // Console.WriteLine("Please enter a secret key for the symmetric algorithm.");
+                //var key = Console.ReadLine();
+
+                Console.WriteLine("Please enter a string for encryption");
+                var str = Console.ReadLine();
+                var encryptedString = AesOperation.EncryptString(key, str);
+                Console.WriteLine($"encrypted string = {encryptedString}");
+
+                var decryptedString = AesOperation.DecryptString(key, encryptedString);
+                Console.WriteLine($"decrypted string = {decryptedString}");
+
+                //try
+                // {
+                //     int zero = 0;
+                //     int result = 5 / zero;
+                // }
+                // catch (DivideByZeroException ex)
+                // {
+                //     Logger logger = LogManager.GetLogger("fileLogger");
+                //     logger.Error(ex);
+                // }
+
+
+                Console.ReadKey();
+            }
             Console.Read();
         }
     }
